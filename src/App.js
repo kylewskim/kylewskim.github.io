2df5fd5e6f99a8cd5d87a7d98900b4a06abb1f79
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-14 23:17:16 +0900

    router exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {HashRouter, Route} from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import Home from './routes/Home';
+import About from  './routes/About';
+import Bright from  './routes/Bright';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  return <HashRouter>
+    {/* <ScrollToTop /> */}
+    {/* <Navigation/> */}
+    <Route path='/' exact={true} component= {Home} />
+    <Route path='/about' component= {About} />
+    <Route path='/bright' component= {Bright} />
+  </HashRouter>
+
 }
 
 export default App;
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import Button from "@mui/material/Button";
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <Button variant="contained" color="primary"> Hello World</Button>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+// import Grid from '@mui/material/Grid';
+// import './Home.css';
+// import { Link } from 'react-router-dom';
+
+// function Home() {
+
+// }
+
+// export default Home;
