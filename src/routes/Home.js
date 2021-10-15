a9b2702d3596460a7c634e5d2091428ea4552d8a
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-15 23:50:27 +0900

    nav font updated

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -10,7 +10,7 @@ function Home() {
                     <text> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
                     </Grid>
                     <Grid item xs={12} sm={6}>
-                    <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation" style={{marginTop: 14}}/>
+                    <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
                     <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
                         <Grid className='nav_content' item xs={12} sm={6}> Project </Grid>
                         <Grid className='nav_content' item xs={12} sm={6}> About </Grid>
