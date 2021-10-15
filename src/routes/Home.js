872f995b65b290a397a512104c38bc91769cdcef
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-15 23:26:46 +0900

    nav updated

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,23 @@
-// import React from 'react';
-// import Grid from '@mui/material/Grid';
-// import './Home.css';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import './Home.css';
+import { Link } from 'react-router-dom';
 
-// function Home() {
+function Home() {
+    return (
+        <Grid container className='grid-container' spacing={4} direction='row' justify='flex-start' alignItems='flex-start'>
+                    <Grid className='name-wooseongkim'item xs={12} sm={6}>
+                    <text> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                    <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation" style={{marginTop: 14}}/>
+                    <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
+                        <Grid className='nav_content' item xs={12} sm={6}> Project </Grid>
+                        <Grid className='nav_content' item xs={12} sm={6}> About </Grid>
+                    </Grid>   
+                </Grid>
+        </Grid>
+    )
+}
 
-// }
-
-// export default Home;
+export default Home;
