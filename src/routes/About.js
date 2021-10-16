f0988799c87de5f806d7523c4eefa4119b0c3a63
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-16 23:19:44 +0900

    font size updated

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,8 +6,8 @@ import { Link } from 'react-router-dom';
 function About() {
     return (
         <div className='About'>
-            <Grid container className='grid-container' spacing={4} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
-                <Grid item xs={6} sm={6}>
+            <Grid container className='grid-container' spacing={6} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
+                <Grid item xs={12} sm={6}>
                     <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
                 </Grid>
                 <Grid className='name-wooseongkim'item xs={12} sm={6}>
@@ -16,7 +16,7 @@ function About() {
                 <Grid item xs={12} sm={6}>
                     <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
                         <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'none' , color: 'black' }}> Project </Link> </Grid> 
-                        <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'line-through' , color: 'black' }}> About </Link> </Grid> 
+                        <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'line-through' , color: 'black' }}> About / CV </Link> </Grid> 
                     </Grid>
                 </Grid>
             </Grid>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,22 +6,22 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className='Home'>
-            <Grid container className='grid-container' spacing={4} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
-                <Grid item xs={6} sm={6}>
+            <Grid container className='grid-container' spacing={6} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
+                <Grid item xs={12} sm={6}>
                     <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
                 </Grid>
                 <Grid className='name-wooseongkim'item xs={12} sm={6}>
-                    <text> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
+                    <text className='text-name-wooseongkim'> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
                         <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'line-through' , color: 'black' }}> Project </Link> </Grid> 
-                        <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'none' , color: 'black' }}> About </Link> </Grid> 
+                        <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'none' , color: 'black' }}> About / CV </Link> </Grid> 
                     </Grid>
                 </Grid>   
             </Grid>
 
-            <Grid container className='grid-container'  spacing={4} rowSpacing={7.5} direction='row' justify='flex-start' alignItems='flex-start' style={{marginTop: 150, marginBottom: 180}}>
+            <Grid container className='grid-container'  spacing={6} rowSpacing={7.5} direction='row' justify='flex-start' alignItems='flex-start' style={{marginTop: 180, marginBottom: 180}}>
                     <Grid  item  xs={12} sm={6}>
                         <Link to='/bright' className='link-item' style={{ textDecoration: 'none' , color: 'black' }}>
                         <div>
