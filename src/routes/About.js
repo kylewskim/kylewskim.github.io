90902a7902904c14219d197d0deecbec83fb6f3d
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-16 12:27:14 +0900

    about nav updated

---
+++ src/routes/About.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import './About.css';
+import { Link } from 'react-router-dom';
+
+function About() {
+    return (
+        <Grid container className='grid-container' spacing={4} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
+                <Grid item xs={6} sm={6}>
+                    <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
+                </Grid>
+                <Grid className='name-wooseongkim'item xs={12} sm={6}>
+                    <text> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
+                </Grid>
+                    <Grid item xs={12} sm={6}>
+                    <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
+                        <Grid className='nav_content' item xs={6}> <Link to='/'> Project </Link> </Grid> 
+                        <Grid className='nav_content' item xs={6}> <Link to='/About'> About </Link> </Grid> 
+                    </Grid>
+                    </Grid>   
+
+                
+        </Grid>
+    )
+}
+
+export default About;
