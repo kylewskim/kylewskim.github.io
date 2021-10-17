1ba347183e5a483ac081be55c2ebfd7a260ad41a
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-17 23:08:38 +0900

    margin updated

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,19 +6,19 @@ import { Link } from 'react-router-dom';
 function About() {
     return (
         <div className='About'>
-            <Grid container className='grid-container' spacing={6} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
+            <Grid container className='nav-grid-container' spacing={8} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
                 <Grid item xs={12} sm={6}>
                     <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
                 </Grid>
                 <Grid className='name-wooseongkim'item xs={12} sm={6}>
-                    <text> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
+                    <text className='text-name-wooseongkim'> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
+                    <Grid container className='nav-selection' id='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
                         <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'none' , color: 'black' }}> Project </Link> </Grid> 
                         <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'line-through' , color: 'black' }}> About / CV </Link> </Grid> 
                     </Grid>
-                </Grid>
+                </Grid>   
             </Grid>
         </div>
     )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className='Home'>
-            <Grid container className='grid-container' spacing={6} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
+            <Grid container className='nav-grid-container' spacing={8} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
                 <Grid item xs={12} sm={6}>
                     <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
                 </Grid>
@@ -14,14 +14,14 @@ function Home() {
                     <text className='text-name-wooseongkim'> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
                 </Grid>
                 <Grid item xs={12} sm={6}>
-                    <Grid container className='nav-selection' id='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
+                    <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
                         <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'line-through' , color: 'black' }}> Project </Link> </Grid> 
                         <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'none' , color: 'black' }}> About / CV </Link> </Grid> 
                     </Grid>
                 </Grid>   
             </Grid>
 
-            <Grid container className='grid-container' spacing={6} rowSpacing={7.5} direction='row' justify='flex-start' alignItems='flex-start' style={{marginTop: 100, marginBottom: 100}}>
+            <Grid container className='item-grid-container' spacing={8} rowSpacing={7.5} direction='row' justify='flex-start' alignItems='flex-start' style={{marginTop: 100, marginBottom: 100}}>
                     <Grid  item className='frame-item' xs={12} sm={6}>
                         <Link to='/bright' className='link-item' style={{ textDecoration: 'none' , color: 'black' }}>
                         <div>
@@ -50,6 +50,15 @@ function Home() {
                         </Link>
                     </Grid>
             </Grid>
+
+            <Grid container className='info-grid-container' spacing={8} direction='row' justify='flex-start' alignItems='flex-start' style={{marginTop: 100, marginBottom: 200}}>
+                <Grid item className='info-wooseongkim' xs={9} sm={5}>
+                    <p className='text-info-wooseongkim'>
+                    Wooseong Kim is UX Designer based in South Korea. He is a senior Industrial Design student with computer science and entrepreneurs minor from UNIST (Ulsan National Institute of Science and Technology), South Korea. As an undergraduate student, with a striving for learning, KAIST (Korea Institute of Science and Technology), HKUST (Hong Kong University of Science and Technology). 
+                    </p>
+                </Grid>
+            </Grid>
+
         </div>
     )
 }
