f00ba4fe613f8e6b01575bf1f3ef400bfa27da29
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-10-25 18:17:23 +0900

    nav updated

--- src/routes/About.js
+++ src/routes/About.js
@@ -6,19 +6,25 @@ import { Link } from 'react-router-dom';
 function About() {
     return (
         <div className='About'>
-            <Grid container className='nav-grid-container' spacing={8} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
-                <Grid item xs={12} sm={6}>
-                    <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
-                </Grid>
-                <Grid className='name-wooseongkim'item xs={12} sm={6}>
+            <Grid container className='nav-grid-container' spacing={8} direction='row-reverse' justifyContent='flex-end' alignItems='flex-end'>
+                <Grid className='name-wooseongkim' item xs={12} sm={6}>
                     <text className='text-name-wooseongkim'> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Grid container className='nav-selection' id='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
-                        <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'none' , color: 'black' }}> Project </Link> </Grid> 
+                <Grid item xs={12} sm={6} justifyContent='flex-end' alignItems='flex-end'>
+                    <Grid container className='nav-selection' spacing={1} direction='row' justifyContent='flex-end' alignItems='flex-end'>
+                        <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'none' , color: 'black'}}> Project </Link> </Grid> 
                         <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'line-through' , color: 'black' }}> About / CV </Link> </Grid> 
                     </Grid>
                 </Grid>   
+            </Grid>
+
+            <Grid container className='item-grid-container' spacing={8} rowSpacing={7.5} direction='row' justify='flex-start' alignItems='flex-start' style={{marginTop: 100, marginBottom: 100}}>
+                <Grid className='about' item xs={12} sm={6}>
+                    About
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    About
+                </Grid>
             </Grid>
         </div>
     )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -6,15 +6,12 @@ import { Link } from 'react-router-dom';
 function Home() {
     return (
         <div className='Home'>
-            <Grid container className='nav-grid-container' spacing={8} direction='row-reverse' justify='flex-start' alignItems='flex-start'>
-                <Grid item xs={12} sm={6}>
-                    <img className="nav_bar" src={process.env.PUBLIC_URL + '/assets/Home/home_gradation.png'} alt="nav_gradation"/>
-                </Grid>
+            <Grid container className='nav-grid-container' spacing={8} direction='row' justify='flex-end' alignItems='flex-end'>
                 <Grid className='name-wooseongkim'item xs={12} sm={6}>
                     <text className='text-name-wooseongkim'> Wooseong Kim </text> <br /> <text className='ux-designer'> UX Designer </text>
                 </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-start' alignItems='flex-start'>
+                <Grid item xs={12} sm={6} justify='flex-end' alignItems='flex-end'>
+                    <Grid container className='nav-selection' spacing={1} direction='row' justify='flex-end' alignItems='flex-end'>
                         <Grid className='nav_content' item xs={6}> <Link to='/' style={{ textDecoration: 'line-through' , color: 'black' }}> Project </Link> </Grid> 
                         <Grid className='nav_content' item xs={6}> <Link to='/About' style={{ textDecoration: 'none' , color: 'black' }}> About / CV </Link> </Grid> 
                     </Grid>
